dd9fc673344fd1d7fd9dac4489a601df77f6dcb3
rahulandhe183 <randhe@uncc.edu>
2021-10-01 00:43:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const axios = require('axios');
 const port = 3000;
 
+app.get('/', function(req,res){
+    res.send("Go to http://143.198.125.67:3000/say?keyword=Hello for AWS function response");
+});
+
+
 app.get('/say', function(req,res) {
         resp = "Hello World" + req.query.keyword ;
         axios.get('https://rb75mu1n56.execute-api.us-east-1.amazonaws.com/default/myFunction?keyword='+req.query.keyword)
